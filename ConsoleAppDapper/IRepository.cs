6f62fc2df64a9e89b5a26634a3928056b7338ce8
DAVID MIKE <aidipifr@hotmail.com>
2019-10-21 22:59:07 +0200

    Add project files.

---
+++ ConsoleAppDapper/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ConsoleAppDapper
+{
+    public interface IRepository<T> //where T : EntityBase, IAggregateRoot
+    {
+        void Add(T item);
+        void Remove(T item);
+        void Update(T item);
+        T FindByID(Guid id);
+        IEnumerable<T> Find(Expression<Func<T, bool>> predicate);
+        IEnumerable<T> FindAll();
+    }
+}
